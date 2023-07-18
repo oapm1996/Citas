aa9e3eb1cd03503ae67d02907ed8f38f1b2cb7cb
oapm1996 <isc.oscarpina@gmail.com>
2023-07-17 22:24:33 -0700

    <DesignTailwind>[FrontEnt]: The design was completed

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+export const Paciente = () => {
+  return (
+    <div className="m-3 bg-white px-5 py-10 shadow-md rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+              Nombre: {''}
+              <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+              Propietario: {''}
+              <span className="font-normal normal-case">Oscar P.</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+              Email: {''}
+              <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+              Fecha Altal: {''}
+              <span className="font-normal normal-case">10 - Julio - 2023</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+              Sintomas: {''}
+              <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. In, aliquam maiores? Rerum quae voluptas id accusantium laboriosam. Impedit cupiditate repellat laboriosam odio quia minima, nobis, quod consequuntur tempora, ab possimus?</span>
+            </p>
+          </div>
+  )
+}
